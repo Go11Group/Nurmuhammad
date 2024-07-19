22a808db933b4e91e13cf0e08fc6da9f9abf2d62
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-07-19 13:13:55 +0500

    n

--- casbin/cmd/main.go
+++ casbin/cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"new/api"
+	"new/api/handler"
+	dbcon "new/storage/postgres"
+)
 
+func main() {
+	db, err := dbcon.ConnnectDb()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	user := dbcon.NewUserRepository(db)
+	hand := NewHandler(user)
+	c := api.ConnectGin(hand)
+	c.Run()
+}
+
+func NewHandler(user *dbcon.UserRepo) *handler.Handler {
+	return &handler.Handler{
+		User: user,
+	}
 }
